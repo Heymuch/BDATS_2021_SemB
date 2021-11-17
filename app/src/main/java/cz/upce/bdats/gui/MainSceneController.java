ffeb7fca102fef5320c3b1ba7ca474c841f81383
Jiří Hejduk <hejduk@phoenix.local>
2021-11-17 16:33:39 +0100

    opravdu finální verze, protože už na to nechci v životě chmátnout

--- app/src/main/java/cz/upce/bdats/gui/MainSceneController.java
+++ app/src/main/java/cz/upce/bdats/gui/MainSceneController.java
@@ -11,11 +11,11 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
-
 import cz.upce.bdats.autopujcovna.Auto;
 import cz.upce.bdats.autopujcovna.IAutopujcovna;
 import cz.upce.bdats.autopujcovna.IPobocka;
 import cz.upce.bdats.autopujcovna.IteratorTyp;
+import cz.upce.bdats.autopujcovna.Pobocka;
 import cz.upce.bdats.autopujcovna.Pozice;
 import cz.upce.bdats.data.Generator;
 import cz.upce.bdats.data.Persistence;
@@ -57,14 +57,16 @@ public class MainSceneController implements Initializable {
     private void updateNazevAutopujcovny() {
         gNazevAutopujcovny.setText("");
 
-        if (Objects.isNull(autopujcovna)) return;
+        if (Objects.isNull(autopujcovna))
+            return;
         gNazevAutopujcovny.setText("Autopujčovna " + autopujcovna.getNazev());
     }
 
     private void updatePobocky() throws Exception {
         gPobocky.getItems().clear();
 
-        if (Objects.isNull(autopujcovna)) return;
+        if (Objects.isNull(autopujcovna))
+            return;
         Iterator<IPobocka> it = autopujcovna.iterator(IteratorTyp.POBOCKY);
         while (it.hasNext())
             gPobocky.getItems().add(it.next());
@@ -72,24 +74,28 @@ public class MainSceneController implements Initializable {
         try {
             IPobocka p = autopujcovna.zpristupniPobocku(Pozice.AKTUALNI);
             gPobocky.getSelectionModel().select(p);
-        } catch (Exception e) {}
+        } catch (Exception e) {
+        }
     }
 
     private void updateAutomobily() throws Exception {
         gAutomobily.getItems().clear();
 
         try {
-            if (Objects.isNull(autopujcovna)) return;
+            if (Objects.isNull(autopujcovna))
+                return;
             Iterator<Auto> it = autopujcovna.iterator(IteratorTyp.AUTOMOBILY);
             while (it.hasNext())
                 gAutomobily.getItems().add(it.next());
-        } catch (Exception e) {}
+        } catch (Exception e) {
+        }
     }
 
     private void updateVypujcene() throws Exception {
         gVypujcene.getItems().clear();
 
-        if (Objects.isNull(autopujcovna)) return;
+        if (Objects.isNull(autopujcovna))
+            return;
         Iterator<Auto> it = autopujcovna.iterator(IteratorTyp.VYPUJCENE_AUTOMOBILY);
         while (it.hasNext())
             gVypujcene.getItems().add(it.next());
@@ -101,7 +107,8 @@ public class MainSceneController implements Initializable {
         dialog.setHeaderText("Zadejte SPZ automobilu");
 
         Optional<String> opt = dialog.showAndWait();
-        if (!opt.isPresent()) return null;
+        if (!opt.isPresent())
+            return null;
 
         return opt.get();
     }
@@ -120,7 +127,8 @@ public class MainSceneController implements Initializable {
     void borrowAutomobil(ActionEvent event) {
         try {
             String spz = handleSPZ();
-            if (Objects.isNull(spz) || spz.length() == 0) return;
+            if (Objects.isNull(spz) || spz.length() == 0)
+                return;
 
             autopujcovna.vypujcAuto(spz);
             updateAutomobily();
@@ -316,7 +324,8 @@ public class MainSceneController implements Initializable {
     void removeAutomobil(ActionEvent event) {
         try {
             String spz = handleSPZ();
-            if (Objects.isNull(spz) || spz.length() == 0) return;
+            if (Objects.isNull(spz) || spz.length() == 0)
+                return;
 
             autopujcovna.odeberAuto(spz);
             updateAutomobily();
@@ -421,10 +430,43 @@ public class MainSceneController implements Initializable {
     void selectAutomobil(ActionEvent event) {
         try {
             String spz = handleSPZ();
-            if (Objects.isNull(spz) || spz.length() == 0) return;
+            if (Objects.isNull(spz) || spz.length() == 0)
+                return;
 
             Auto a = autopujcovna.zpristupniAuto(spz);
             gAutomobily.getSelectionModel().select(a);
+        } catch (Exception e) {
+            handleException(e);
+        }
+    }
+
+    @FXML
+    void selectAutomobilAll(ActionEvent event) {
+        try {
+            String spz = handleSPZ();
+            if (Objects.isNull(spz) || spz.length() == 0) return;
+
+            Iterator<IPobocka> pobocky = autopujcovna.iterator(IteratorTyp.POBOCKY);
+            Auto a = null;
+            IPobocka p = null;
+            while (pobocky.hasNext()) {
+                try {
+                    p = pobocky.next();
+                    a = p.zpristupniAuto(spz);
+                    break;
+                } catch (Exception e) {}
+            }
+
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("Vyhledání automobilu");
+            if (Objects.nonNull(a)) {
+                alert.setHeaderText("Automobil nalezen: pobočka " + p.toString());
+                alert.setContentText(a.toString());
+            } else {
+                alert.setHeaderText("Automobil nenalezen");
+                alert.setContentText("Hledaná SPZ: " + spz);
+            }
+            alert.showAndWait();
         } catch (Exception e) {
             handleException(e);
         }
